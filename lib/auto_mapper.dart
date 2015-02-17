11ab3e9668a20f09ffdb8aa95c923964d303c1f9
jrote1 <jrote1@me.com>
2015-02-17 15:56:02 +0000

    Code Refactoring

--- lib/auto_mapper.dart
+++ lib/auto_mapper.dart
@@ -5,56 +5,56 @@ import 'dart:mirrors';
 class AutoMapper
 {
 
-	bool isPrimitive( dynamic value )
+	bool _isPrimitive( dynamic value )
 	{
 		return (value == null || value is bool || value is num || value is String || value is DateTime || value is double);
 	}
 
-	Iterable<DeclarationMirror> getVariableMirrors( InstanceMirror fromMirror )
+	Iterable<DeclarationMirror> _getVariableMirrors( InstanceMirror fromMirror )
 	{
 		var fromVariables = fromMirror.type.declarations.values.where( ( d )
 																	   => d is VariableMirror );
 		return fromVariables;
 	}
 
-	String getVariableName( VariableMirror variableMirror )
+	String _getVariableName( VariableMirror variableMirror )
 	{
 		return MirrorSystem.getName( variableMirror.simpleName );
 	}
 
-	instantiateType( Type toType )
+	dynamic _instantiateType( Type toType )
 	{
 		var resultList = reflectClass( toType ).newInstance( const Symbol( '' ), [] ).reflectee;
 		return resultList;
 	}
 
-	_mapList( Type toType, from )
+	dynamic _mapList( dynamic from, Type toType )
 	{
-		var resultList = instantiateType( toType );
+		var resultList = _instantiateType( toType );
 		var listObjectType = reflectType( toType ).typeArguments[0].reflectedType;
 		resultList.addAll( from.map( ( x )
 									 => map( x, listObjectType ) ) );
 		return resultList;
 	}
 
-	_mapCustomType( Type toType, from )
+	dynamic _mapCustomType( dynamic from, Type toType )
 	{
-		var result = instantiateType( toType );
+		var result = _instantiateType( toType );
 
 		var fromMirror = reflect( from );
 		var toMirror = reflect( result );
 
-		var fromVariables = getVariableMirrors( fromMirror );
-		var toVariables = getVariableMirrors( toMirror );
+		var fromVariables = _getVariableMirrors( fromMirror );
+		var toVariables = _getVariableMirrors( toMirror );
 
 		for ( var variableMirror in fromVariables )
 		{
 			if ( toVariables.any( ( v )
-								  => propertyNamesMatch( v, variableMirror ) ) )
+								  => _propertyNamesMatch( v, variableMirror ) ) )
 			{
 				var destinationVariable = toVariables
 				.firstWhere( ( v )
-							 => propertyNamesMatch( v, variableMirror ) );
+							 => _propertyNamesMatch( v, variableMirror ) );
 				var fromValue = fromMirror.getField( variableMirror.simpleName ).reflectee;
 
 				toMirror.setField( destinationVariable.simpleName, map( fromValue, destinationVariable.type.reflectedType ) );
@@ -66,21 +66,21 @@ class AutoMapper
 
 	dynamic map( dynamic from, Type toType )
 	{
-		if ( isPrimitive( from ) )
+		if ( _isPrimitive( from ) )
 		{
 			return from;
 		}
 		else if ( from is List )
 		{
-			return _mapList( toType, from );
+			return _mapList( from, toType );
 		}
 		else
 		{
-			return _mapCustomType( toType, from );
+			return _mapCustomType( from, toType );
 		}
 	}
 
-	bool propertyNamesMatch( VariableMirror variableMirror1, VariableMirror variableMirror2 )
-	=> getVariableName( variableMirror1 ) == getVariableName( variableMirror2 );
+	bool _propertyNamesMatch( VariableMirror variableMirror1, VariableMirror variableMirror2 )
+	=> _getVariableName( variableMirror1 ) == _getVariableName( variableMirror2 );
 
 }
